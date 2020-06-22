e3ce8e744fd12fe2bbae5cefb5ceb194e52ca740
Headgrass <headgrass@ngs.ru>
2020-06-22 07:15:56 +0700

    Протестил вывод сообщений о состоянии запуска и воостановления активити

--- Weather/app/src/main/java/ru/geekbrains/weather/Settings.java
+++ Weather/app/src/main/java/ru/geekbrains/weather/Settings.java
@@ -3,14 +3,11 @@ package ru.geekbrains.weather;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import ru.geekbrains.weather.R;
 
 public class Settings extends AppCompatActivity {
 
@@ -19,13 +16,68 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+
         Button back_to_main = findViewById(R.id.backToMain);
         back_to_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Bundle(onSaveInstanceState();)
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+
+        saveInstanceState.getBoolean("Humidity");
+        saveInstanceState.getBoolean("Pressure");
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        CheckBox humidity = findViewById(R.id.checkboxHumidity);
+        saveInstanceState.putBoolean("Humidity", humidity.isChecked());
+        CheckBox pressure = findViewById(R.id.checkboxPressure);
+        saveInstanceState.putBoolean("Pressure", pressure.isChecked());
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
